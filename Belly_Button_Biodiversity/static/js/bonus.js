0f9fdae834cb233f1a1b08c8cf2e7ec1489a238a
Rodrigo Becerril <rodrigo.becerril@clearesult.com>
2018-10-26 12:48:24 -0500

    Main commit

---
+++ Belly_Button_Biodiversity/static/js/bonus.js
@@ -0,0 +1,63 @@
+function buildGauge(wfreq){
+
+    // Trig to calc meter point
+    var degrees = 180 - wfreq*20, 
+    radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+    pathX = String(x),
+    space = ' ',
+    pathY = String(y),
+    pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+    var dataGauge = [
+        { 
+            type: 'scatter',
+            x: [0], y:[0],
+            marker: {size: 28, color:'850000'},
+            showlegend: false,
+            name: 'WFREQ',
+            text: wfreq,
+            hoverinfo: 'text+name'
+        },
+        { 
+            values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+            rotation: 90,
+            text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+            textinfo: 'text',
+            textposition:'inside',
+            marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(38, 139, 25, .5)',
+                             'rgba(81, 162, 70, .5)', 'rgba(103, 173, 94, .5)', 
+                             'rgba(124, 185, 117, .5)', 'rgba(146, 197, 140, .5)', 
+                             'rgba(168, 208, 163, .5)', 'rgba(189, 220, 186, .5)', 
+                             'rgba(211, 231, 209, .5)', 'rgba(255, 255, 255, 0)']},
+            labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+            hoverinfo: 'label',
+            hole: .5,
+            type: 'pie',
+            showlegend: false
+    }];
+
+    var layout = {
+        shapes:[
+            {
+                type: 'path',
+                path: path,
+                fillcolor: '850000',
+                line: { color: '850000' }
+            }],
+        title: '<b>Belly Button Washing Frequency</b><br>Scrubs per Week',
+        //height: 1000,
+        //width: 1000,
+        xaxis: {zeroline:false, showticklabels:false,showgrid: false, range: [-1, 1]},
+        yaxis: {zeroline:false, showticklabels:false,showgrid: false, range: [-1, 1]}
+    };
+
+    //gauge
+    Plotly.newPlot("gauge", dataGauge, layout);
+}
